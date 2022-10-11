c55623ca7d415b449b319bdca5505a72a4aa9ed5
ByteHamster <info@bytehamster.com>
2022-10-11 13:29:59 +0200

    Test very space efficient solutions only

--- benchmark/RecSplitContender.h
+++ benchmark/RecSplitContender.h
@@ -46,22 +46,17 @@ class RecSplitContender : public Contender {
 
 template <int l>
 void recSplitTestMulti(size_t N) {
-    {RecSplitContender<l>(N, l).run();}
-    {RecSplitContender<l>(N, 2 * l).run();}
-    {RecSplitContender<l>(N, 50).run();}
     {RecSplitContender<l>(N, 100).run();}
-    {RecSplitContender<l>(N, 250).run();}
     {RecSplitContender<l>(N, 500).run();}
-    {RecSplitContender<l>(N, 1000).run();}
     {RecSplitContender<l>(N, 2000).run();}
 }
 
 void recSplitContenderRunner(size_t N) {
-    recSplitTestMulti<2>(N);
-    recSplitTestMulti<3>(N);
-    recSplitTestMulti<4>(N);
-    recSplitTestMulti<5>(N);
     recSplitTestMulti<6>(N);
     recSplitTestMulti<7>(N);
     recSplitTestMulti<8>(N);
+    recSplitTestMulti<11>(N);
+    recSplitTestMulti<12>(N);
+    recSplitTestMulti<13>(N);
+    recSplitTestMulti<14>(N);
 }
--- benchmark/ShockHashContender.h
+++ benchmark/ShockHashContender.h
@@ -46,21 +46,18 @@ class ShockHashContender : public Contender {
 
 template <int l>
 void shockHashTestMulti(size_t N) {
-    {ShockHashContender<l>(N, l).run();}
-    {ShockHashContender<l>(N, 2 * l).run();}
-    {ShockHashContender<l>(N, 50).run();}
     {ShockHashContender<l>(N, 100).run();}
-    {ShockHashContender<l>(N, 250).run();}
     {ShockHashContender<l>(N, 500).run();}
-    {ShockHashContender<l>(N, 1000).run();}
     {ShockHashContender<l>(N, 2000).run();}
 }
 
 void shockHashContenderRunner(size_t N) {
-    shockHashTestMulti<7>(N);
-    shockHashTestMulti<8>(N);
     shockHashTestMulti<12>(N);
     shockHashTestMulti<15>(N);
-    shockHashTestMulti<17>(N);
-    shockHashTestMulti<20>(N);
+    shockHashTestMulti<24>(N);
+    shockHashTestMulti<26>(N);
+    shockHashTestMulti<28>(N);
+    shockHashTestMulti<30>(N);
+    shockHashTestMulti<32>(N);
+    shockHashTestMulti<34>(N);
 }
