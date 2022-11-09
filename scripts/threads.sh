dc642375a8696b4559c23a47809c50b8da975a9a
ByteHamster <info@bytehamster.com>
2022-11-09 13:56:06 +0100

    Multi-threaded measurements

--- scripts/threads.sh
+++ scripts/threads.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 hostname
-strings ComparisonThreads | grep fPIC
+strings ComparisonN | grep fPIC
 
 for i in $(seq 0 8 64); do
   threads=$i
   if [[ $threads == 0 ]]; then
     threads=1
   fi
-  ./ComparisonThreads --numKeys 10M --numThreads "$threads" --iterations 2
+  ./ComparisonN --numKeys "$((10*threads))M" --numThreads "$threads" --iterations 2 --minimalOnly
 done
