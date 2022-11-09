ffb090a7b2e494409a2ebab9a6d7609236d0dce3
ByteHamster <info@bytehamster.com>
2022-11-09 16:19:36 +0100

    Strong and weak scaling

---
+++ scripts/scalingStrong.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+hostname
+strings ComparisonN | grep fPIC
+
+for i in $(seq 1 1 8); do
+  threads=$i
+  if [[ $threads == 0 ]]; then
+    threads=1
+  fi
+  ./ComparisonN --numKeys 20M --numThreads "$threads" --iterations 2 --numQueries 0 --minimalOnly
+done
