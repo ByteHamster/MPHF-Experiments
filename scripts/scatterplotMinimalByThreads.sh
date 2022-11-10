b501a846fa2287be61efdc85470d14d50771b788
ByteHamster <info@bytehamster.com>
2022-11-10 16:02:34 +0100

    Don't re-run sequential algorithms

--- scripts/scatterplotMinimalByThreads.sh
+++ scripts/scatterplotMinimalByThreads.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+# shellcheck disable=SC2086
 hostname
 strings Comparison | grep fPIC
 
-for threads in 1 4 8; do
-  ./Comparison --numKeys "$((10*threads))M" --numThreads "$threads" --numQueries 0 --loadFactor 0.9   --sichash --pthash --chd
-  ./Comparison --numKeys "$((10*threads))M" --numThreads "$threads" --numQueries 0 --loadFactor 0.95  --sichash --pthash --chd
-  ./Comparison --numKeys "$((10*threads))M" --numThreads "$threads" --numQueries 0 --loadFactor 0.97  --sichash --pthash --chd
-  ./Comparison --numKeys "$((10*threads))M" --numThreads "$threads" --numQueries 0 --recsplit --bbhash
-done
+params="--numKeys 10M --numThreads 1 --numQueries 0"
+./Comparison $params --loadFactor 0.9   --sichash --pthash --chd
+./Comparison $params --loadFactor 0.95  --sichash --pthash --chd
+./Comparison $params --loadFactor 0.97  --sichash --pthash --chd
+./Comparison $params --recsplit --simdrecsplit --bbhash
+
+params="--numKeys 40M --numThreads 4 --numQueries 0"
+./Comparison $params --loadFactor 0.9  --pthash
+./Comparison $params --loadFactor 0.95 --pthash
+./Comparison $params --loadFactor 0.97 --pthash
+./Comparison $params --simdrecsplit --bbhash
+
+params="--numKeys 80M --numThreads 8 --numQueries 0"
+./Comparison $params --loadFactor 0.9  --pthash
+./Comparison $params --loadFactor 0.95 --pthash
+./Comparison $params --loadFactor 0.97 --pthash
+./Comparison $params --simdrecsplit --bbhash
