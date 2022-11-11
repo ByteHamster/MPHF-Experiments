eb4fb26a2a2da75602eb20177a4c01d972b136dd
ByteHamster <info@bytehamster.com>
2022-11-11 09:23:47 +0100

    More measurements

--- benchmark/RecSplitContender.h
+++ benchmark/RecSplitContender.h
@@ -46,12 +46,16 @@ class RecSplitContender : public Contender {
 
 template <int l>
 void recSplitTestMulti(size_t N) {
+    {RecSplitContender<l>(N, l).run();}
+    {RecSplitContender<l>(N, 50).run();}
     {RecSplitContender<l>(N, 100).run();}
     {RecSplitContender<l>(N, 500).run();}
     {RecSplitContender<l>(N, 2000).run();}
 }
 
 void recSplitContenderRunner(size_t N) {
+    recSplitTestMulti<4>(N);
+    recSplitTestMulti<5>(N);
     recSplitTestMulti<6>(N);
     recSplitTestMulti<7>(N);
     recSplitTestMulti<8>(N);
--- benchmark/SIMDRecSplitContender.h
+++ benchmark/SIMDRecSplitContender.h
@@ -46,12 +46,19 @@ class SIMDRecSplitContender : public Contender {
 
 template <int l>
 void simdRecSplitTestMulti(size_t N) {
+    {SIMDRecSplitContender<l>(N, l).run();}
+    {SIMDRecSplitContender<l>(N, 50).run();}
     {SIMDRecSplitContender<l>(N, 100).run();}
     {SIMDRecSplitContender<l>(N, 500).run();}
     {SIMDRecSplitContender<l>(N, 2000).run();}
 }
 
 void simdRecSplitContenderRunner(size_t N) {
+    simdRecSplitTestMulti<4>(N);
+    simdRecSplitTestMulti<5>(N);
+    simdRecSplitTestMulti<6>(N);
+    simdRecSplitTestMulti<7>(N);
+    simdRecSplitTestMulti<8>(N);
     simdRecSplitTestMulti<9>(N);
     simdRecSplitTestMulti<10>(N);
     simdRecSplitTestMulti<11>(N);
