ac58da5a86810c302cf2ac1416ba2891aab8714e
ByteHamster <info@bytehamster.com>
2022-11-17 09:28:26 +0100

    Add fast configuration

--- comparisonQueries.cpp
+++ comparisonQueries.cpp
@@ -7,7 +7,6 @@
 
 int main(int argc, char** argv) {
     size_t N = 5e6;
-    size_t iterations = 1;
     tlx::CmdlineParser cmd;
     cmd.add_bytes('n', "numKeys", N, "Number of objects");
     cmd.add_bytes('q', "numQueries", Contender::numQueries, "Number of queries to perform");
@@ -16,18 +15,18 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    for (size_t i = 0; i < iterations; i++) {
-        {PTHashContender<true, pthash::compact_compact>(N, 0.99, 7.0).run();}
-        {PTHashContender<true, pthash::dictionary_dictionary >(N, 0.88, 11.0).run();}
-        {PTHashContender<true, pthash::elias_fano >(N, 0.99, 6.0).run();}
-        {SicHashContender<true, 64>(N, 0.97, sichash::SicHashConfig().percentages(0.45, 0.31)).run();}
-        {SicHashContender<true, 64>(N, 0.9, sichash::SicHashConfig().percentages(0.21, 0.78)).run();}
-        {SIMDRecSplitContender<8>(N, 100).run();}
-        {SIMDRecSplitContender<16>(N, 2000).run();}
-        {RecSplitContender<8>(N, 100).run();}
-        {RecSplitContender<16>(N, 2000).run();}
-        {BBHashContender(N, 1.0, 0).run();}
-        {BBHashContender(N, 5.0, 0).run();}
-    }
+    {SIMDRecSplitContender<5>(N, 5).run();}
+    {SIMDRecSplitContender<8>(N, 100).run();}
+    {SIMDRecSplitContender<16>(N, 2000).run();}
+    {RecSplitContender<5>(N, 5).run();}
+    {RecSplitContender<8>(N, 100).run();}
+    {RecSplitContender<16>(N, 2000).run();}
+    {BBHashContender(N, 1.0, 0).run();}
+    {BBHashContender(N, 5.0, 0).run();}
+    {PTHashContender<true, pthash::compact_compact>(N, 0.99, 7.0).run();}
+    {PTHashContender<true, pthash::dictionary_dictionary >(N, 0.88, 11.0).run();}
+    {PTHashContender<true, pthash::elias_fano >(N, 0.99, 6.0).run();}
+    {SicHashContender<true, 64>(N, 0.97, sichash::SicHashConfig().percentages(0.45, 0.31)).run();}
+    {SicHashContender<true, 64>(N, 0.9, sichash::SicHashConfig().percentages(0.21, 0.78)).run();}
     return 0;
 }
