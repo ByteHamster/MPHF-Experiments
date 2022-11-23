31be3a2451f1445ffb90d499e82c9ad94bfd5ccc
ByteHamster <info@bytehamster.com>
2022-11-23 12:37:31 +0100

    Actually run new multi-threaded measurements

--- scripts/scatterplotMinimalByThreads.sh
+++ scripts/scatterplotMinimalByThreads.sh
@@ -4,19 +4,19 @@ hostname
 strings Comparison | grep fPIC
 
 params="--numKeys 10M --numThreads 1 --numQueries 0"
-./Comparison $params --loadFactor 0.9   --sichash --pthash --chd
-./Comparison $params --loadFactor 0.95  --sichash --pthash --chd
-./Comparison $params --loadFactor 0.97  --sichash --pthash --chd
+./Comparison $params --loadFactor 0.9   --sichash --pthash --chd --partitionedSichash --partitionedPthash
+./Comparison $params --loadFactor 0.95  --sichash --pthash --chd --partitionedSichash --partitionedPthash
+./Comparison $params --loadFactor 0.97  --sichash --pthash --chd --partitionedSichash --partitionedPthash
 ./Comparison $params --recsplit --simdrecsplit --bbhash
 
 params="--numKeys 40M --numThreads 4 --numQueries 0"
-./Comparison $params --loadFactor 0.9  --pthash
-./Comparison $params --loadFactor 0.95 --pthash
-./Comparison $params --loadFactor 0.97 --pthash
+./Comparison $params --loadFactor 0.9  --pthash --partitionedSichash --partitionedPthash
+./Comparison $params --loadFactor 0.95 --pthash --partitionedSichash --partitionedPthash
+./Comparison $params --loadFactor 0.97 --pthash --partitionedSichash --partitionedPthash
 ./Comparison $params --simdrecsplit --bbhash
 
 params="--numKeys 80M --numThreads 8 --numQueries 0"
-./Comparison $params --loadFactor 0.9  --pthash
-./Comparison $params --loadFactor 0.95 --pthash
-./Comparison $params --loadFactor 0.97 --pthash
+./Comparison $params --loadFactor 0.9  --pthash --partitionedSichash --partitionedPthash
+./Comparison $params --loadFactor 0.95 --pthash --partitionedSichash --partitionedPthash
+./Comparison $params --loadFactor 0.97 --pthash --partitionedSichash --partitionedPthash
 ./Comparison $params --simdrecsplit --bbhash
