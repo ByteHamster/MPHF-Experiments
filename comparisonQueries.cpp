dfef974a7e31f2aedb4897e852827bff9650bbe3
ByteHamster <info@bytehamster.com>
2023-07-31 11:32:57 +0200

    Use configurations from ShockHash benchmarks

--- comparisonQueries.cpp
+++ comparisonQueries.cpp
@@ -4,6 +4,7 @@
 #include "benchmark/RustFmphGoContender.h"
 #include "benchmark/RecSplitContender.h"
 #include "benchmark/SIMDRecSplitContender.h"
+#include "benchmark/BBHashContender.h"
 #include "benchmark/ShockHashContender.h"
 
 int main(int argc, char** argv) {
@@ -23,6 +24,8 @@ int main(int argc, char** argv) {
     {RecSplitContender<5>(N, 5).run();}
     {RecSplitContender<8>(N, 100).run();}
     {RecSplitContender<14>(N, 2000).run();}
+    {BBHashContender(N, 1.0, 0).run();}
+    {BBHashContender(N, 5.0, 0).run();}
     {RustFmphGoContender(N, 1.0).run();}
     {RustFmphGoContender(N, 5.0).run();}
     {PTHashContender<true, pthash::compact_compact>(N, 0.99, 7.0).run();}
@@ -30,8 +33,9 @@ int main(int argc, char** argv) {
     {PTHashContender<true, pthash::elias_fano >(N, 0.99, 6.0).run();}
     {SicHashContender<true, 64>(N, 0.97, sichash::SicHashConfig().percentages(0.45, 0.31)).run();}
     {SicHashContender<true, 64>(N, 0.9, sichash::SicHashConfig().percentages(0.21, 0.78)).run();}
-    {ShockHashContender<20>(N, 50).run();}
+    {ShockHashContender<30>(N, 100).run();}
     {ShockHashContender<30>(N, 2000).run();}
-    {ShockHashContender<44>(N, 2000).run();}
+    {ShockHashContender<39>(N, 2000).run();}
+    {ShockHashContender<58>(N, 2000).run();}
     return 0;
 }
