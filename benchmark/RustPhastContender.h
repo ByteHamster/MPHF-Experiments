809c5e2ee50de8b2b2a4dd6effca2abb520f0108
Piotr Beling <piotr.beling@wmii.uni.lodz.pl>
2025-03-20 11:04:16 +0100

    upgraded ph to 0.9 and added PHast support

---
+++ benchmark/RustPhastContender.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#include "RustContender.h"
+
+extern "C" {
+    void *createPhastStruct(uint64_t len, const char **str);
+    void constructPhast(void *rustStruct, uint8_t bits_per_seed, uint16_t bucket_size100);
+    uint64_t queryPhast(void *rustStruct, const char *key, size_t length);
+    uint64_t queryPhast8(void *rustStruct, const char *key, size_t length);
+    size_t sizePhast(void *rustStruct);
+    void destroyPhastStruct(void *rustStruct);
+}
+
+class RustPhastContender : public RustContender {
+    protected:
+        void *rustStruct = nullptr;
+        uint8_t bits_per_seed;
+        uint16_t bucket_size100;
+    public:
+        RustPhastContender(size_t N, uint8_t bits_per_seed, uint16_t bucket_size100)
+            : RustContender(N), bits_per_seed(bits_per_seed), bucket_size100(bucket_size100) {
+        }
+
+        ~RustPhastContender() override {
+            if (rustStruct != nullptr) {
+                destroyPhastStruct(rustStruct);
+                rustStruct = nullptr;
+            }
+        }
+
+        std::string name() override {
+            return std::string("RustPHast")
+                + " bits_per_seed=" + std::to_string(bits_per_seed)
+                + " bucket_size100=" + std::to_string(bucket_size100);
+        }
+
+        void beforeConstruction(const std::vector<std::string> &keys) override {
+            RustContender::beforeConstruction(keys);
+            rustStruct = createPhastStruct(N, data);
+        }
+
+        void construct(const std::vector<std::string> &keys) override {
+            (void) keys;
+            constructPhast(rustStruct, bits_per_seed, bucket_size100);
+        }
+
+        size_t sizeBits() override {
+            return sizePhast(rustStruct) * 8;
+        }
+
+        void performQueries(const std::span<std::string> keys) override {
+            if (bits_per_seed == 8) {
+                auto x = [&] (std::string &key) {
+                    return queryPhast8(rustStruct, key.c_str(), key.length()); };
+                doPerformQueries(keys, x);
+            } else {
+                auto x = [&] (std::string &key) {
+                    return queryPhast(rustStruct, key.c_str(), key.length()); };
+                doPerformQueries(keys, x);
+            }
+        }
+
+        void performTest(const std::span<std::string> keys) override {
+            if (bits_per_seed == 8) {
+                auto x = [&] (std::string &key) {
+                    return queryPhast8(rustStruct, key.c_str(), key.length()); };
+                doPerformTest(keys, x);
+            } else {
+                auto x = [&] (std::string &key) {
+                    return queryPhast(rustStruct, key.c_str(), key.length()); };
+                doPerformTest(keys, x);
+            }
+        }
+};
+
+void rustPHastContenderRunner(size_t N) {
+    RustPhastContender(N, 6, 320).run();
+    RustPhastContender(N, 7, 370).run();
+    RustPhastContender(N, 8, 410).run();
+    RustPhastContender(N, 8, 430).run();
+    RustPhastContender(N, 8, 450).run();
+    RustPhastContender(N, 9, 510).run();
+    RustPhastContender(N, 9, 530).run();
+    RustPhastContender(N, 10, 620).run();
+    RustPhastContender(N, 10, 650).run();
+}
