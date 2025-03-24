367b4225e94136b69d07b7ba66529bcc7f57936d
ByteHamster <info@bytehamster.com>
2025-03-24 14:11:04 +0100

    More FCH configs

--- contenders/bucketplacement/FchContender.cpp
+++ contenders/bucketplacement/FchContender.cpp
@@ -1,7 +1,7 @@
 #include "FchContender.h"
 
 void fchPtHashContenderRunner(size_t N) {
-    for (double c = 4.0; c >= 2.5; c -= 0.1) {
+    for (double c = 1.0; c >= 4.0; c += 0.2) {
         FchContender(N, c).run();
     }
 }
