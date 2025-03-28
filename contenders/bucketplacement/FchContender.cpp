b0df9d3b2919fb36a2e76bb6e25641946aba2fc4
ByteHamster <info@bytehamster.com>
2025-03-28 11:21:12 +0100

    Add more configurations

--- contenders/bucketplacement/FchContender.cpp
+++ contenders/bucketplacement/FchContender.cpp
@@ -1,7 +1,7 @@
 #include "FchContender.h"
 
 void fchPtHashContenderRunner(size_t N) {
-    for (double c = 4.0; c >= 2.5; c -= 0.1) {
+    for (double c = 7.0; c >= 2.4; c -= 0.2) {
         FchContender(N, c).run();
     }
 }
--- contenders/bucketplacement/PartitionedPTHashContender.cpp
+++ contenders/bucketplacement/PartitionedPTHashContender.cpp
@@ -6,9 +6,11 @@ void partitionedPtHashContenderRunner(size_t N, double loadFactor, bool minimalO
             PartitionedPTHashContender<false, pthash::elias_fano>(N, loadFactor, lambda).run();
             PartitionedPTHashContender<false, pthash::dictionary_dictionary>(N, loadFactor, lambda).run();
             PartitionedPTHashContender<false, pthash::dictionary_elias_fano>(N, loadFactor, lambda).run();
+            PartitionedPTHashContender<false, pthash::compact_compact>(N, loadFactor, lambda).run();
         }
         PartitionedPTHashContender<true, pthash::elias_fano>(N, loadFactor, lambda).run();
         PartitionedPTHashContender<true, pthash::dictionary_dictionary>(N, loadFactor, lambda).run();
         PartitionedPTHashContender<true, pthash::dictionary_elias_fano>(N, loadFactor, lambda).run();
+        PartitionedPTHashContender<true, pthash::compact_compact>(N, loadFactor, lambda).run();
     }
 }
