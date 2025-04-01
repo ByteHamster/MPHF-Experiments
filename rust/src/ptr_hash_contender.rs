11b860a7a75a77231abca15e239db88d47b90e99
Piotr Beling <piotr.beling@wmii.uni.lodz.pl>
2025-04-01 14:07:47 +0200

    PtrHash upgrade and performance fix

--- rust/src/ptr_hash_contender.rs
+++ rust/src/ptr_hash_contender.rs
@@ -1,22 +1,25 @@
 use mem_dbg::SizeFlags;
 use ptr_hash::PtrHashParams;
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
 use std::os::raw::c_char;
 use std::slice;
 use std::hint::black_box;
 
-type PtrHashLinearVec = ptr_hash::PtrHash<Box<[u8]>, ptr_hash::bucket_fn::Linear,
-    Vec<u32>, ptr_hash::hash::FxHash, Vec<u8>>; // Fast
-type PtrHashSquareVec = ptr_hash::PtrHash<Box<[u8]>, ptr_hash::bucket_fn::SquareEps,
-    Vec<u32>, ptr_hash::hash::FxHash, Vec<u8>>;
-type PtrHashCubicVec = ptr_hash::PtrHash<Box<[u8]>, ptr_hash::bucket_fn::CubicEps,
-    Vec<u32>, ptr_hash::hash::FxHash, Vec<u8>>;
-type PtrHashLinearEF = ptr_hash::PtrHash<Box<[u8]>, ptr_hash::bucket_fn::Linear,
-    ptr_hash::CachelineEfVec, ptr_hash::hash::FxHash, Vec<u8>>;
-type PtrHashSquareEF = ptr_hash::PtrHash<Box<[u8]>, ptr_hash::bucket_fn::SquareEps,
-    ptr_hash::CachelineEfVec, ptr_hash::hash::FxHash, Vec<u8>>;
-type PtrHashCubicEF = ptr_hash::PtrHash<Box<[u8]>, ptr_hash::bucket_fn::CubicEps,
-    ptr_hash::CachelineEfVec, ptr_hash::hash::FxHash, Vec<u8>>; // Compact
+use cacheline_ef::CachelineEfVec;
+
+type PtrHashLinearVec = ptr_hash::PtrHash<&'static [u8], ptr_hash::bucket_fn::Linear,
+    Vec<u32>, ptr_hash::hash::Xx64, Vec<u8>>; // Fast
+type PtrHashSquareVec = ptr_hash::PtrHash<&'static [u8], ptr_hash::bucket_fn::SquareEps,
+    Vec<u32>, ptr_hash::hash::Xx64, Vec<u8>>;
+type PtrHashCubicVec = ptr_hash::PtrHash<&'static [u8], ptr_hash::bucket_fn::CubicEps,
+    Vec<u32>, ptr_hash::hash::Xx64, Vec<u8>>;
+type PtrHashLinearEF = ptr_hash::PtrHash<&'static [u8], ptr_hash::bucket_fn::Linear,
+    CachelineEfVec, ptr_hash::hash::Xx64, Vec<u8>>;
+type PtrHashSquareEF = ptr_hash::PtrHash<&'static [u8], ptr_hash::bucket_fn::SquareEps,
+    CachelineEfVec, ptr_hash::hash::Xx64, Vec<u8>>;
+type PtrHashCubicEF = ptr_hash::PtrHash<&'static [u8], ptr_hash::bucket_fn::CubicEps,
+    CachelineEfVec, ptr_hash::hash::Xx64, Vec<u8>>; // Compact
 
 pub enum PtrHashVariant {
     None,
@@ -41,32 +44,32 @@ pub extern "C" fn constructPtrHash(struct_ptr: *mut PtrHashVariant, keys_ptr: *c
         1 => {
             let mut params = PtrHashParams::default_fast();
             params.lambda = lambda;
-            PtrHashVariant::LinearVec( PtrHashLinearVec::new(&keys[..], params) )
+            PtrHashVariant::LinearVec( PtrHashLinearVec::new_from_par_iter(keys.len(), keys.par_iter().map(|v| v.as_ref()), params) )
         },
         2 => {
             let mut params = PtrHashParams::default_square();
             params.lambda = lambda;
-            PtrHashVariant::SquareVec( PtrHashSquareVec::new(&keys[..], params) )
+            PtrHashVariant::SquareVec( PtrHashSquareVec::new_from_par_iter(keys.len(), keys.par_iter().map(|v| v.as_ref()), params) )
         },
         3 => {
             let mut params = PtrHashParams::default_compact();
             params.lambda = lambda;
-            PtrHashVariant::CubicVec( PtrHashCubicVec::new(&keys[..], params) )
+            PtrHashVariant::CubicVec( PtrHashCubicVec::new_from_par_iter(keys.len(), keys.par_iter().map(|v| v.as_ref()), params) )
         },
         4 => {
             let mut params = PtrHashParams::default_fast();
             params.lambda = lambda;
-            PtrHashVariant::LinearEF( PtrHashLinearEF::new(&keys[..], params) )
+            PtrHashVariant::LinearEF( PtrHashLinearEF::new_from_par_iter(keys.len(), keys.par_iter().map(|v| v.as_ref()), params) )
         },
         5 => {
             let mut params = PtrHashParams::default_square();
             params.lambda = lambda;
-            PtrHashVariant::SquareEF( PtrHashSquareEF::new(&keys[..], params) )
+            PtrHashVariant::SquareEF( PtrHashSquareEF::new_from_par_iter(keys.len(), keys.par_iter().map(|v| v.as_ref()), params) )
         },
         6 => {
             let mut params = PtrHashParams::default_compact();
             params.lambda = lambda;
-            PtrHashVariant::CubicEF( PtrHashCubicEF::new(&keys[..], params) )
+            PtrHashVariant::CubicEF( PtrHashCubicEF::new_from_par_iter(keys.len(), keys.par_iter().map(|v| v.as_ref()), params) )
         },
         _ => panic!("Invalid variant"),
     }
@@ -75,14 +78,13 @@ pub extern "C" fn constructPtrHash(struct_ptr: *mut PtrHashVariant, keys_ptr: *c
 #[no_mangle]
 pub extern "C" fn queryPtrHash(struct_ptr: *const PtrHashVariant, key_c_s: *const c_char, length: usize) -> u64 {
     let key = unsafe { slice::from_raw_parts(key_c_s as *const u8, length) };
-    let key = key.to_owned().into_boxed_slice();
     match unsafe { &*struct_ptr } {
-        PtrHashVariant::LinearVec(f) => f.index_minimal(&key) as u64,
-        PtrHashVariant::SquareVec(f) => f.index_minimal(&key) as u64,
-        PtrHashVariant::CubicVec(f) => f.index_minimal(&key) as u64,
-        PtrHashVariant::LinearEF(f) => f.index_minimal(&key) as u64,
-        PtrHashVariant::SquareEF(f) => f.index_minimal(&key) as u64,
-        PtrHashVariant::CubicEF(f) => f.index_minimal(&key) as u64,
+        PtrHashVariant::LinearVec(f) => f.index(&key) as u64,
+        PtrHashVariant::SquareVec(f) => f.index(&key) as u64,
+        PtrHashVariant::CubicVec(f) => f.index(&key) as u64,
+        PtrHashVariant::LinearEF(f) => f.index(&key) as u64,
+        PtrHashVariant::SquareEF(f) => f.index(&key) as u64,
+        PtrHashVariant::CubicEF(f) => f.index(&key) as u64,
         PtrHashVariant::None => panic!("PtrHash not constructed yet"),
     }
 }
@@ -92,17 +94,17 @@ pub extern "C" fn queryPtrHashAll(struct_ptr: *const PtrHashVariant, keys_ptr: *
     let keys = unsafe { &*keys_ptr }; 
     match unsafe { &*struct_ptr } {
         PtrHashVariant::LinearVec(f) =>
-            for key in keys { black_box(f.index_minimal(key)); },
+            for key in keys { black_box(f.index(&key.as_ref())); },
         PtrHashVariant::SquareVec(f) =>
-            for key in keys { black_box(f.index_minimal(key)); },
+            for key in keys { black_box(f.index(&key.as_ref())); },
         PtrHashVariant::CubicVec(f) =>
-            for key in keys { black_box(f.index_minimal(key)); },
+            for key in keys { black_box(f.index(&key.as_ref())); },
         PtrHashVariant::LinearEF(f) =>
-            for key in keys { black_box(f.index_minimal(key)); },
+            for key in keys { black_box(f.index(&key.as_ref())); },
         PtrHashVariant::SquareEF(f) =>
-            for key in keys { black_box(f.index_minimal(key)); },
+            for key in keys { black_box(f.index(&key.as_ref())); },
         PtrHashVariant::CubicEF(f) =>
-            for key in keys { black_box(f.index_minimal(key)); },
+            for key in keys { black_box(f.index(&key.as_ref())); },
         PtrHashVariant::None => panic!("PtrHash not constructed yet"),
     };
 }
